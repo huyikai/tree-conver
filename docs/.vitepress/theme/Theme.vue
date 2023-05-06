a78e96db68eafb96f1405bcfc74c37ce8048f66b
huyikai <hyk_tiger@foxmail.com>
2023-05-06 10:45:29 +0800

    :art: format of the code

--- docs/.vitepress/theme/Theme.vue
+++ docs/.vitepress/theme/Theme.vue
@@ -3,9 +3,9 @@ import VPLayout from './Layout.vue';
 import Home from './components/home.vue';
 </script>
 <template>
-    <VPLayout>
-        <template #custom-home>
-            <Home></Home>
-        </template>
-    </VPLayout>
+  <VPLayout>
+    <template #custom-home>
+      <Home></Home>
+    </template>
+  </VPLayout>
 </template>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -8,16 +8,16 @@ import 'vitepress/dist/client/theme-default/styles/components/vp-code.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css';
 import 'vitepress/dist/client/theme-default/styles/components/vp-sponsor.css';
-import './styles/style.scss'
+import './styles/style.scss';
 import NotFound from 'vitepress/dist/client/theme-default/NotFound.vue';
 import VPBadge from 'vitepress/dist/client/theme-default/components/VPBadge.vue';
 const theme = {
-    Layout,
-    NotFound,
-    enhanceApp: async ({ app }) => {
-        // app is the Vue 3 app instance from `createApp()`. router is VitePress'
-        // custom router. `siteData`` is a `ref`` of current site-level metadata.
-        app.component('Badge', VPBadge);
-      }
+  Layout,
+  NotFound,
+  enhanceApp: async ({ app }) => {
+    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
+    // custom router. `siteData`` is a `ref`` of current site-level metadata.
+    app.component('Badge', VPBadge);
+  }
 };
 export default theme;
