079513a02b4bb5ebe00c2bfd519de4350f24d982
huyikai <hyk_tiger@foxmail.com>
2023-05-06 15:27:39 +0800

    :sparkles: can custom fields

--- packages/tree-conver/src/node/arrayToTree.ts
+++ packages/tree-conver/src/node/arrayToTree.ts
@@ -1,31 +1,39 @@
 interface Node {
   id: string; // 节点的唯一标识符
-  name: string; // 节点的名字
   children?: Array<Node>; // 节点的子节点数组
   pid?: string; // 节点的父节点标识符
 }
-export const arrayToTree = (array: Array<Node>) => {
-  const map = array.reduce((acc: Record<string, Node>, node) => {
-    // 构建节点id到节点的映射
-    acc[node.id] = { ...node, children: [] }; // 将当前节点加入映射中
+interface Options {
+  idKey?: string; // 自定义 id 字段的名称
+  pidKey?: string; // 自定义 pid 字段的名称
+  childrenKey?: string; // 自定义 children 字段的名称
+}
+
+export const arrayToTree = (array: Array<Node>, options: Options = {}) => {
+  // 解构 options 对象来获取自定义字段名称或默认值
+  const { idKey = 'id', pidKey = 'pid', childrenKey = 'children' } = options;
+  // 创建节点 id 到节点的映射
+  const map = array.reduce((acc: Record<string, Node>, node: any) => {
+    acc[node[idKey]] = { ...node, [childrenKey]: [] }; // 初始化节点并添加到映射中
     return acc;
   }, {});
 
-  Object.values(map).forEach((node) => {
+  Object.values(map).forEach((node: any) => {
     // 遍历所有节点
-    const parent = node.pid !== undefined ? map[node.pid] : undefined; // 找到当前节点的父节点
-    if (parent) {
+    const parentId = node[pidKey];
+    if (parentId) {
       // 如果存在父节点
-      if (!parent.children) {
-        // 如果父节点没有children属性
-        parent.children = []; // 则初始化children属性为一个数组
+      const parent: any = map[parentId]; // 获取当前节点的父节点
+      if (!parent[childrenKey]) {
+        // 如果父节点没有 children 属性
+        parent[childrenKey] = []; // 初始化 children 属性
       }
-      parent.children.push(node); // 将当前节点加入父节点的children数组中
+      parent[childrenKey].push(node); // 将当前节点添加到父节点的 children 数组中
     }
   });
 
   // 找到所有根节点（没有父节点的节点）并构建树
-  const tree = Object.values(map).filter((node) => !node.pid);
+  const tree = Object.values(map).filter((node: any) => !node[pidKey]);
 
   return tree;
 };
